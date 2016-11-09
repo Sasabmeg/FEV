9f7f091d77dc967d8dabb204753f41f3b51483ad
Balthasar <brewer109@gmx.de>
2016-11-09 19:27:12 +0100

    Added cycle colored frm animation export feature
    Works like the usual way when you press "Exp anim". To export a cycle colored frm animation you have to have the "Color cycle" checkbox checked. 
    
    I tried to stick to the programs structure and yet there may be some things TODO or at least to check:
    
    - by now 5 frames will be exported (= 5 single png-files) --> not sure if every cc animated frm only has 5
    - by now only initial direction (0) will be exported --> not sure if there are cc animated frm files which have more then one available direction.

---
+++ FrmExporter.java
@@ -0,0 +1,84 @@
+package net.fodev.tools.frm.control;
+
+import java.awt.image.BufferedImage;
+import java.awt.image.IndexColorModel;
+import java.io.File;
+import java.io.IOException;
+import javax.imageio.ImageIO;
+import javafx.scene.image.Image;
+import net.fodev.tools.frm.model.ColorCycleOffset;
+import net.fodev.tools.frm.model.FoPalette;
+import net.fodev.tools.frm.model.FrmFrame;
+import net.fodev.tools.frm.model.Header;
+
+public class FrmExporter {
+	public static void exportSingleFrameToFile(Header header, int direction, int frameIndex, String filename, boolean hasBackground) throws IOException {
+		try {
+			Image image = header.getImage(direction, frameIndex, hasBackground);
+			FrmImageConverter.writeImageToBmpFile(image, filename);
+		} catch (Exception ex) {
+			ex.printStackTrace();
+			return;
+		}
+	}
+
+	public static void exportSingleColorCycleFrameToFile(Header header, int direction, int frameIndex, String filename, ColorCycleOffset cco, boolean hasBackground) throws IOException {
+        try {            
+            FrmFrame frame = (FrmFrame) header.getFrame(0);
+            
+            byte[] image = FrmImageConverter.getSingleColorCycledImage(frame.getData(), frame.getWidth(),
+                    frame.getHeight(), 0, cco, hasBackground);
+            FrmImageConverter.writeCycledImageToFile(image, filename);
+            
+        } catch (Exception ex) {
+            ex.printStackTrace();
+            return;
+        }
+    }
+	
+	public static void exportColorCycleAnimationToFile(Header header, String folderName, String frameFileName, boolean hasBackground) throws IOException {
+        String newFolderName = folderName + "/" + frameFileName;
+        new File(newFolderName).mkdirs();
+        ColorCycleOffset cco = new ColorCycleOffset();
+         
+        //TODO: Check if Color Cycled Images always only have one direction.
+        for (int direction = 0; direction < 1; direction++) {
+          //TODO: Check whether Color Cycled imaged always only have 5 frames or not.
+            for (int index = 0; index < 5; index++) {
+                exportSingleColorCycleFrameToFile(header, direction, index, newFolderName + "/" + frameFileName + "_" + index + ".png", cco, hasBackground);
+                cco.step();
+            }
+        }
+    }
+	
+	public static void exportAnimationToFile(Header header, String folderName, String frameFileName, boolean hasBackground) throws IOException {
+		String newFolderName = folderName + "/" + frameFileName;
+		new File(newFolderName).mkdirs();
+		for (int direction = 0; direction < header.getTotalFrames() / header.getFramesPerDirection(); direction++) {
+			for (int index = 0; index < header.getFramesPerDirection(); index++) {
+				exportSingleFrameToFile(header, direction, index, newFolderName + "/" + direction + "_" + index + ".png", hasBackground);
+			}
+		}
+	}
+
+	public static void exportDefaultFoPaletteIntoFile(String filename) throws IOException {
+		BufferedImage bufferedImage = new BufferedImage(256, 256, BufferedImage.TYPE_INT_RGB);
+		IndexColorModel foColorModel = FoPalette.getDefaultColorModel(true);
+		for (int i = 0; i < 16; i ++) {
+			for (int j = 0; j < 16; j++) {
+				int index = i * 16 + j;
+				int red = foColorModel.getRed(index);
+				int green = foColorModel.getGreen(index);
+				int blue = foColorModel.getBlue(index);
+				int rgb = 65536 * red + 256 * green + blue;
+				for (int x = 0; x < 16; x++) {
+					for (int y = 0; y < 16; y++) {
+						bufferedImage.setRGB(j * 16 + y, i * 16 + x, rgb);
+					}
+				}
+			}
+		}
+		File outputfile = new File(filename);
+		ImageIO.write(bufferedImage, "png", outputfile);
+	}
+}
---
+++ FrmImageConverter.java
@@ -0,0 +1,111 @@
+package net.fodev.tools.frm.control;
+
+import java.awt.Graphics2D;
+import java.awt.image.BufferedImage;
+import java.awt.image.DataBuffer;
+import java.awt.image.DataBufferByte;
+import java.awt.image.IndexColorModel;
+import java.awt.image.Raster;
+import java.awt.image.RenderedImage;
+import java.awt.image.SampleModel;
+import java.awt.image.WritableRaster;
+import java.io.ByteArrayInputStream;
+import java.io.ByteArrayOutputStream;
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
+
+import javax.imageio.ImageIO;
+
+import javafx.embed.swing.SwingFXUtils;
+import javafx.scene.image.Image;
+import net.fodev.tools.frm.model.ColorCycleOffset;
+import net.fodev.tools.frm.model.FoPalette;
+
+public class FrmImageConverter {
+	public static Image getJavaFXImage(byte[] rawPixels, int width, int height, int offset, boolean hasBackground) {
+		ByteArrayOutputStream out = new ByteArrayOutputStream();
+		try {
+			ImageIO.write((RenderedImage) createBufferedImage(rawPixels, width, height, offset, null, hasBackground), "png", out);
+			out.flush();
+		} catch (IOException ex) {
+			ex.printStackTrace();
+		}
+		ByteArrayInputStream in = new ByteArrayInputStream(out.toByteArray());
+		return new javafx.scene.image.Image(in);
+	}
+
+	//for export
+	public static byte[] getSingleColorCycledImage(byte[] rawPixels, int width, int height, 
+	        int offset, ColorCycleOffset colorCycleOffset, boolean hasBackground) throws IOException {
+	    ByteArrayOutputStream out = new ByteArrayOutputStream();
+	    
+	    try {
+            ImageIO.write((RenderedImage) createBufferedImage(rawPixels, width, height, offset, colorCycleOffset, hasBackground), "png", out);
+            out.flush();
+        } catch (IOException ex) {            
+            ex.printStackTrace();
+        }
+	    byte[] result = out.toByteArray();
+	    out.close();
+        return result;	    
+	}
+	
+	public static void writeCycledImageToFile(byte[] image, String filename) throws IOException {	    
+	    //System.out.println("file: " + filename);
+	    FileOutputStream outputF = new FileOutputStream(new File(filename));
+        outputF.write(image);
+        outputF.close();
+	}
+	
+	public static Image getJavaFXImageWithColorCycle(byte[] rawPixels, int width, int height, int offset, ColorCycleOffset colorCycleOffset, boolean hasBackground) {
+		ByteArrayOutputStream out = new ByteArrayOutputStream();
+		try {
+			ImageIO.write((RenderedImage) createBufferedImage(rawPixels, width, height, offset, colorCycleOffset, hasBackground), "png", out);			
+			out.flush();
+		} catch (IOException ex) {
+			ex.printStackTrace();
+		}
+		ByteArrayInputStream in = new ByteArrayInputStream(out.toByteArray());
+		return new javafx.scene.image.Image(in);
+
+	}
+
+	private static BufferedImage createBufferedImage(byte[] pixels, int width, int height, int offset, ColorCycleOffset cco, boolean hasBackground) {
+		SampleModel sm = getIndexSampleModel(width, height, cco, hasBackground);
+		DataBuffer db = new DataBufferByte(pixels, width * height * 2, offset);
+		WritableRaster raster = Raster.createWritableRaster(sm, db, null);
+		IndexColorModel cm;
+		if (cco == null) {
+			cm = FoPalette.getDefaultColorModel(hasBackground);
+		} else {
+			cm = FoPalette.getAnimatedDefaultColorModel(cco, hasBackground);
+		}
+		BufferedImage image = new BufferedImage(cm, raster, false, null);
+		return image;
+	}
+
+	private static SampleModel getIndexSampleModel(int width, int height, ColorCycleOffset cco, boolean hasBackground) {
+		IndexColorModel icm;
+		if (cco == null) {
+			icm = FoPalette.getDefaultColorModel(hasBackground);
+		} else {
+			icm = FoPalette.getAnimatedDefaultColorModel(cco, hasBackground);
+		}
+		WritableRaster wr = icm.createCompatibleWritableRaster(1, 1);
+		SampleModel sampleModel = wr.getSampleModel();
+		sampleModel = sampleModel.createCompatibleSampleModel(width, height);
+		return sampleModel;
+	}
+
+	public static void writeImageToBmpFile(Image image, String filename) throws IOException {
+		BufferedImage bufferedImage = SwingFXUtils.fromFXImage(image, null);
+		// Remove alpha-channel from buffered image:
+		BufferedImage imageRGB = new BufferedImage(bufferedImage.getWidth(), bufferedImage.getHeight(), BufferedImage.OPAQUE);
+		Graphics2D graphics = imageRGB.createGraphics();
+		graphics.drawImage(bufferedImage, 0, 0, null);
+		ImageIO.write(imageRGB, FrmUtils.getFileExtension(filename), new File(filename));
+		graphics.dispose();
+	}
+
+}

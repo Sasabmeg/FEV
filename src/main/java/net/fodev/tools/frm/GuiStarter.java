73387e4c287e315881b1ec04e86287a516e739d0
Sasabmeg <eeeeeeediot@gmail.com>
2023-04-28 14:08:40 +0200

    Added fix when running with different versions of Java/JavaFX. There was a bug in 17? version where one must do a wrapper to call main code from, if it's an extension of JavaFX application it would fail when run from single executable jar.

---
+++ src/main/java/net/fodev/tools/frm/GuiStarter.java
@@ -0,0 +1,9 @@
+package net.fodev.tools.frm;
+
+import net.fodev.tools.frm.view.MainFrame;
+
+public class GuiStarter {
+    public static void main(final String[] args) {
+        MainFrame.main(args);
+    }
+}
